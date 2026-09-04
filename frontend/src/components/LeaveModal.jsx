49f74b7637c74654e7edae130032fc0070ddc16c
pashupati <web5@decostyle.co.in>
2026-09-04 11:34:45 +0530

    changes on min date

--- frontend/src/components/LeaveModal.jsx
+++ frontend/src/components/LeaveModal.jsx
@@ -377,7 +377,7 @@ export const LeaveModal = ({ user, onClose, onSuccess, showToast }) => {
                   ...inputStyle,
                   opacity: leaveType === "Mis-Punch" ? 0.7 : 1,
                 }}
-                min={leaveType === "Mis-Punch" ? undefined : todayString}
+                // min={leaveType === "Mis-Punch" ? undefined : todayString}
                 readOnly={leaveType === "Mis-Punch"}
               />
             </div>
@@ -390,7 +390,7 @@ export const LeaveModal = ({ user, onClose, onSuccess, showToast }) => {
                   value={endDate}
                   onChange={(e) => setEndDate(e.target.value)}
                   style={inputStyle}
-                  min={startDate || todayString}
+                  // min={startDate || todayString}
                 />
               </div>
             )}
